513430f952f9861f1c38257f8c352cbbce0253e0
dolyajenya <109440290+dolyajenya@users.noreply.github.com>
2024-10-17 21:00:15 +0300

    Inject the forecast HTML from JaveScript

--- src/index.js
+++ src/index.js
@@ -98,3 +98,33 @@ getButton.addEventListener("click", janeLocation);
 form.addEventListener("submit", searchTemp);
 celsiusTemperature.addEventListener("click", celTemp);
 fahrenheitTemperature.addEventListener("click", fahTemp);
+
+
+// Forecast
+function displayForecast () {
+  let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml = 
+      forecastHtml +
+    `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15°</strong>
+              </div>
+            <div class="weather-forecast-temperature">9°</div>
+          </div>
+        </div>   
+    `;
+  });
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+
+}
+
+displayForecast();
